1bd37a0e51568132bffd82f4a44b74d2de59b969
Franco Rezabek <rezabekf@amazon.co.uk>
2020-03-03 20:01:08 +0000

    Fix ebs mount script mount point
    + fix permission for mount script

--- scripts/ec2-mount-ebs-volume.sh
+++ scripts/ec2-mount-ebs-volume.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
-mkdir -p /var/data
+mkdir -p /data
 while [ ! -b $(readlink -f /dev/xvdh) ]; do echo "waiting for device /dev/xvdh"; sleep 5 ; done
 blkid $(readlink -f /dev/xvdh) || mkfs -t ext4 $(readlink -f /dev/xvdh)
 e2label $(readlink -f /dev/xvdh) dcc-data
-grep -q ^LABEL=dcc-data /etc/fstab || echo 'LABEL=dcc-data /var/data ext4 defaults' >> /etc/fstab
-grep -q "^$(readlink -f /dev/xvdh) /var/data " /proc/mounts || mount /var/data
+grep -q ^LABEL=dcc-data /etc/fstab || echo 'LABEL=dcc-data /data ext4 defaults' >> /etc/fstab
+grep -q "^$(readlink -f /dev/xvdh) /data " /proc/mounts || mount /data
